3b36c0b953718e7ae40084778ad5b9c9b5cd58be
Student <Student@G302APC20.manila.dlsu.edu.ph>
2018-05-25 18:24:57 +0800

    Fixed mother bear

--- UVA/10945/Main.java
+++ UVA/10945/Main.java
@@ -4,24 +4,17 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 class Main{
 
-    public static boolean isPal(String s){
-        int end = s.length();
-        for(int i=0; i<(s.length())/2; i++){
-            if(s.charAt(i) != s.charAt(end-i-1))
-                return false;
-        }
-        return true;
-    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         for(String input = br.readLine(); !input.equals("DONE"); input = br.readLine()){
-            input = input.replaceAll("[,.?!\\s]", "");
-            input = input.toUpperCase();
-
-            if(isPal(input)){
+            input = input.replaceAll("[^A-Za-z]", "").toUpperCase();
+			StringBuilder sb = new StringBuilder(input);
+			
+            if(sb.reverse().toString().equals(input)){
                 System.out.println("You won't be eaten!");
             }else{
                 System.out.println("Uh oh..");
