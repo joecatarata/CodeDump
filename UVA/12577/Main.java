fe294ebdf2289454c25c0ba058c039d03e9d3709
Student <Student@G302APC20.manila.dlsu.edu.ph>
2018-05-25 18:13:58 +0800

    Fixed 12577

--- UVA/12577/Main.java
+++ UVA/12577/Main.java
@@ -1,20 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 
-class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int caseCtr = 1;
-		int operations = Integer.parseInt(br.readLine());
-		int accountBalance = 0;
-		while((operations--) > 0){
-			String[] input = br.readLine().split(" ");
-			if(input[0].equalsIgnoreCase("donate")){
-				accountBalance += Integer.parseInt(input[1]);
-			}else if(input[0].equalsIgnoreCase("report")){
-				System.out.println(accountBalance);
-			}
-		}
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseCtr = 1;
+        for(String word = br.readLine(); !word.equals("*"); word = br.readLine()){
+            System.out.print("Case " + caseCtr + ": ");
+            caseCtr++;
+            if(word.equals("Hajj")){
+                System.out.println("Hajj-e-Akbar");
+            }else{
+                System.out.println("Hajj-e-Asghar");
+            }
+        }
+    }
 }
